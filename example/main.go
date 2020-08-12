07d1c7ed4c0e3c8ca7f22f6fc8946f036a298a32
vitmovie <vitales95@mail.ru>
2020-08-12 15:28:32 +0300

    add example how to collect articles

--- example/main.go
+++ example/main.go
@@ -1,9 +1,18 @@
-package confluencego
+package main
 
 import (
 	"fmt"
+	confCollector "github.com/VitMovie/Confluence-Collector"
+)
+
+const (
+	username = "username"
+	token  = "token"
+	url = "https://linkerbot.atlassian.net/wiki/rest/api/content?spaceKey=TS&type=page&start=0&end=99999"
 )
 
 func main() {
-	fmt.Println("hello")
+	// Collect articles with confluence id and title
+	art := confCollector.CollectArticles(username, token, url)
+	fmt.Println(art)
 }
