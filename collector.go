ded8599245d5e287743acda0b6bc8ab2029762a5
vitmovie <vitales95@mail.ru>
2020-08-13 18:54:02 +0300

    update collector with confluence url for each article

--- collector.go
+++ collector.go
@@ -2,17 +2,24 @@ package confluencego
 
 import (
 	"encoding/json"
+	articlesgo "github.com/vitmovie/articlesgo"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"time"
 )
 
-type article map[string]string
+const articlesPath = ".atlassian.net/wiki/rest/api/content?spaceKey=TS&type=page&start=0&end=99999"
 
 var myClient = &http.Client{Timeout: 10 * time.Second}
 
-func getResponse(username, token, url string) interface{} {
+func articlesUrl(domain string) string {
+	return "https://" + domain + articlesPath
+}
+
+func getResponse(username, token, domain string) (interface{}, string) {
 	client := &http.Client{}
+	url := articlesUrl(domain)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		panic(err)
@@ -28,26 +35,33 @@ func getResponse(username, token, url string) interface{} {
 	if err = json.Unmarshal(data, &jsonResp); err != nil {
 		panic(err)
 	}
-	return jsonResp["results"]
+	links := jsonResp["_links"].(map[string]interface{})
+	return jsonResp["results"], links["base"].(string)
 }
 
-func getArticles(in interface{}) []article {
-	var articles []article
+func getArticles(in interface{}, baseUrl string) []articlesgo.Article {
+	var articles []articlesgo.Article
 	switch results := in.(type) {
 	case []interface {}:
 		for _, result := range results {
 			data := result.(map[string]interface{})
-			a := make(article)
-			a["id"] = data["id"].(string)
-			a["title"] = data["title"].(string)
+			a := articlesgo.Article{}
+			confId, err := strconv.Atoi(data["id"].(string))
+			if err != nil {
+				panic(err)
+			}
+			a.ConfluenceID = confId
+			a.Title = data["title"].(string)
+			links := data["_links"].(map[string]interface{})
+			a.Url = baseUrl + links["webui"].(string)
 			articles = append(articles, a)
 		}
 	}
 	return articles
 }
 
-func CollectArticles(username, token, url string) []article {
-	jsonResp := getResponse(username, token, url)
-	articles := getArticles(jsonResp)
+func CollectArticles(username, token, domain string) []articlesgo.Article {
+	jsonResp, baseUrl := getResponse(username, token, domain)
+	articles := getArticles(jsonResp, baseUrl)
 	return articles
 }
